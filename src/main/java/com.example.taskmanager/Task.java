fa5d1067c2ae4561799e2453cc3d446b96694662
ShAsHi <shashinarmala29@gmail.com>
2025-01-19 13:09:33 +0530

    Create Task.java

---
+++ src/main/java/com.example.taskmanager/Task.java
@@ -0,0 +1,22 @@
+package com.example.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+}
